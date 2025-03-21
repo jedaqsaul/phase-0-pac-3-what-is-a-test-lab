c7517a8bc0768d3b47feac1c0f57582a658b4b53
jedidiah <jedaqsaul@gmail.com>
2025-03-21 23:35:25 +0300

    Completed the phase-0-pac-3-what-is-a-test-lab

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan"; // Changed from "Joe" to "Susan"
+const height = 30; // Changed from 74 to a value less than 40
 const message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message};
+module.exports = { name, height, message };
